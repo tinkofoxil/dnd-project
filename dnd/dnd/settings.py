ce52551fca5c068f329ad38314bafc7cb017bf4b
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-16 14:38:09 +0300

    created users and profile apps

--- dnd/dnd/settings.py
+++ dnd/dnd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'dnd_profile.apps.DndProfileConfig',
 ]
 
 MIDDLEWARE = [

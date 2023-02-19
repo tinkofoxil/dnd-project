f3ae180dac37e05079c8c2303be49fd28f14d163
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-19 22:18:06 +0300

    remake ProfileViewSet

--- dnd/dnd_profile/urls.py
+++ dnd/dnd_profile/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+
+router = routers.SimpleRouter()
+router.register(r'profile', views.ProfileViewSet)
+
 urlpatterns = [
-    path('api/v1/profile_list/', views.ProfileAPIList.as_view()),
-    path('api/v1/profile_list/<int:pk>/', views.ProfileAPIUpdate.as_view())
+    path('api/v1/', include(router.urls)),
+    #path('api/v1/profile/', views.ProfileViewSet.as_view({'get': 'list'})),
+    #path('api/v1/profile/<int:pk>/', views.ProfileViewSet.as_view({'put': 'update'}))
 ]
--- dnd/dnd_profile/views.py
+++ dnd/dnd_profile/views.py
@@ -1,16 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import mixins
 
 from .models import Profile
 from .serializer import ProfileSerializer
 
 
-class ProfileAPIList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-
-
-class ProfileAPIUpdate(generics.UpdateAPIView):
+class ProfileViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.ListModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer

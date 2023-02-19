27b8db5fb41188a9629b2ac32508b4f6c421de1d
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-19 18:58:09 +0300

    add listapiview

--- dnd/dnd_profile/urls.py
+++ dnd/dnd_profile/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/profile_list/', views.ProfileAPIView.as_view()),
+    path('api/v1/profile_list/', views.ProfileAPIList.as_view()),
 ]
--- dnd/dnd_profile/views.py
+++ dnd/dnd_profile/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Profile
 from .serializer import ProfileSerializer
 
 
-class ProfileAPIView(generics.ListAPIView):
+class ProfileAPIList(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer

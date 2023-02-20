e7b4929566b15a5fc47261c009bcfc8de226858f
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-20 13:10:14 +0300

    add jwt auth

--- dnd/dnd_profile/urls.py
+++ dnd/dnd_profile/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 from . import views
 
@@ -9,4 +14,7 @@ router.register(r'profile', views.ProfileViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]

db1dd9d7c68d3aa8cf7d2a59cbe58158dbbbd531
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-22 12:58:31 +0300

    edited serializers

--- dnd/dnd_profile/serializer.py
+++ dnd/dnd_profile/serializer.py
@@ -16,14 +16,18 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ("pk", "name", "age", "image",)
 
+    def validate_age(self, age):
+        if type(age) is not int:
+            raise serializers.ValidationError(
+                'Возраст должен быть целочисленным'
+            )
+        elif age <= 0:
+            raise serializers.ValidationError(
+                'Возраст должен быть больше нуля'
+            )
+
     def validate_user(self, user):
         if user.is_anonymous:
             raise serializers.ValidationError(
                     'Юзер не может быть анонимным'
                 )
-
-
-class ProfileShortSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ("pk", "name", "age", "image")

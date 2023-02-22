5df977cdda8a2ed0ecb6b4c5a13194b4ca0000c3
Герман Кабачков <german_kabachkov@mail.ru>
2023-02-22 12:18:07 +0300

    add UserSerializer

--- dnd/dnd_profile/serializer.py
+++ dnd/dnd_profile/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Profile
+from .models import Profile, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Profile
-        fields = ("pk", "name", "age", "image")
+        fields = ("pk", "name", "age", "image", "user")

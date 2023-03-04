17573743dcb58996e6be050743acf1ae14f98fa6
Герман Кабачков <german_kabachkov@mail.ru>
2023-03-04 11:49:51 +0300

    added validating

--- dnd/dnd_profile/serializer.py
+++ dnd/dnd_profile/serializer.py
@@ -26,6 +26,13 @@ class ProfileSerializer(serializers.ModelSerializer):
                 'Возраст должен быть больше нуля'
             )
         return age
+    
+    def validate_level(self, level):
+        if level < 0:
+            raise serializers.ValidationError(
+                'Уровень не может быть отрицательным'
+            )
+        return level
 
     def validate_user(self, user):
         if user.is_anonymous:

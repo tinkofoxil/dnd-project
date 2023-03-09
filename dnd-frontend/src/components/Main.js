b9be142cfa942d9ffc5c190f9d4093b2f4cfe02d
Герман Кабачков <german_kabachkov@mail.ru>
2023-03-09 19:35:51 +0300

    edited styles of main page

--- dnd-frontend/src/components/Main.js
+++ dnd-frontend/src/components/Main.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../css/main.css';
+import character from "../assets/img/character.png"
+import monster from "../assets/img/monster.png"
+import map from "../assets/img/map.png"
 
 function Main() {
   return (
@@ -13,17 +16,17 @@ function Main() {
           <h2>Рекомендуемый контент</h2>
           <div className="featured-content-container">
             <div className="featured-content-item">
-              <img src="https://via.placeholder.com/150" alt="Featured content" />
+              <img src={character} width="25%" alt="Featured content" />
               <h3>Создать персонажа</h3>
               <p>Создавайте и настраивайте своих собственных персонажей DND с помощью нашего простого в использовании character creator.</p>
             </div>
             <div className="featured-content-item">
-              <img src="https://via.placeholder.com/150" alt="Featured content" />
+              <img src={monster} width="25%" alt="Featured content" />
               <h3>Генератор монстра</h3>
               <p>Создавайте уникальных монстров с помощью нашего генератора монстров. Выбирайте из множества способностей и черт характера.</p>
             </div>
             <div className="featured-content-item">
-              <img src="https://via.placeholder.com/150" alt="Featured content" />
+              <img src={map} width="25%" alt="Featured content" />
               <h3>Планировщик приключений</h3>
               <p>Спланируйте свое следующее приключение с помощью нашего планировщика приключений. Создавайте пользовательские встречи и точки построения.</p>
             </div>

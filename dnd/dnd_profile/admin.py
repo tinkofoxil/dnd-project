893519c3044f1662c411343016d16e8104b8f175
Герман Кабачков <german_kabachkov@mail.ru>
2023-03-10 15:57:51 +0300

    created profileAdmin

--- dnd/dnd_profile/admin.py
+++ dnd/dnd_profile/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Profile
 
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'user')
+
+
+admin.site.register(Profile, ProfileAdmin)

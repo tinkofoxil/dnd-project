cff3158c6878bb4f685ab6c5738f0a35f627715d
Герман Кабачков <german_kabachkov@mail.ru>
2023-03-11 18:27:51 +0300

    edited account page

--- dnd-frontend/src/components/Account.js
+++ dnd-frontend/src/components/Account.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LogoutButton from './users/LogoutButton';
+import axios from 'axios';
 import "../css/account.css"
 
 const Account = () => {
 
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get('http://127.0.0.1:8000/api/v1/auth/users/me', {
+          headers: { Authorization: `Bearer ${localStorage.getItem('access')}` }
+        });
+        setUser(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='AccountPage'>
       <h1>Аккаунт</h1>
+      <h3>Имя пользователя: {user.username}</h3>
       <LogoutButton />
     </div>
   );

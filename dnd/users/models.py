53ffb1d64749f0482879d6d86cc604cbb1e1a6c5
Герман Кабачков <german_kabachkov@mail.ru>
2023-04-26 13:46:57 +0300

    created friendship model

--- dnd/users/models.py
+++ dnd/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from ..dnd_profile.models import User
+
+
+class Friendship(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='friends'
+    )
+    friend = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='friended_by',
+        verbose_name='Друг'
+    )
+
+    class Meta:
+        unique_together = ('user', 'friend')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.friend.username}'

c0a2ac08fa9dbcb3f4ba73d0911e38c51a7df205
Герман Кабачков <german_kabachkov@mail.ru>
2023-06-10 17:41:18 +0300

    Добавлена информация о почте в аккаунт

--- dnd-frontend/src/components/Account.js
+++ dnd-frontend/src/components/Account.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import LogoutButton from './users/LogoutButton';
 import axios from 'axios';
-import "../css/account.css"
+import '../css/account.css'
 
 const Account = () => {
 
@@ -28,36 +29,43 @@ const Account = () => {
   }, []);
 
   return (
-    <div>
-    <div className='AccountPage'>
-      <h1>Аккаунт</h1>
-      <h3>Имя пользователя: {user.username}</h3>
-      <LogoutButton />
-    </div>
-    <div>
-      {profile.results?.map(item => (
-        <div className="profile-container" key={item.pk}>
-            <div className="profile-header">
+  <Container>
+    <Row>
+      <Col md={6} className="account-col">
+        <div className='AccountPage'>
+          <h1>Аккаунт</h1>
+          <h3>Почта: {user.email}</h3>
+          <h3>Имя пользователя: {user.username}</h3>
+          <LogoutButton />
+        </div>
+      </Col>
+      <Col md={6} className="profile-col">
+        <div>
+          {profile.results?.map(item => (
+            <div className="profile-container" key={item.pk}>
+              <div className="profile-header">
                 <h1>{item.name}</h1>
                 <img src={item.image} alt="Character Portrait"/>
-            </div>
-            <div className="profile-body">
+              </div>
+              <div className="profile-body">
                 <div className="profile-section">
-                    <h2>Краткая инфа</h2>
-                    <ul>
-                        <li>Возраст: {item.age}</li>  
-                        <li>Раса: {item.race}</li>
-                        <li>Класс: {item.class_name}</li>
-                        <li>Уровень: {item.level}</li>
-                        <li><Link className='profile-link' to={`/profiles/${item.pk}`}>Больше инфы</Link></li>
-                    </ul> 
+                  <h2>Краткая инфа</h2>
+                  <ul>
+                    <li>Возраст: {item.age}</li>  
+                    <li>Раса: {item.race}</li>
+                    <li>Класс: {item.class_name}</li>
+                    <li>Уровень: {item.level}</li>
+                    <li><Link className='profile-link' to={`/profiles/${item.pk}`}>Больше инфы</Link></li>
+                  </ul> 
                 </div>
+              </div>
             </div>
+          ))}
         </div>
-      ))}
-    </div>
-    </div>
-  );
+      </Col>
+    </Row>
+  </Container>
+);  
 };
 
 export default Account;
